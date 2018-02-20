db1d44c0e340ab8d6ce160abfba9cb29f45964e5
Jack-clay96 <claj2_14@uni.worc.ac.uk>
2018-02-20 14:13:52 +0000

    login

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,24 @@
+Backendless.initApp("BEC3A11B-2A08-4B8F-FF29-4F33380A3900","3C8378C2-F2B4-F4C9-FFCE-F0C165EBFB00");
 document.addEventListener("deviceready", onDeviceReady, false);
 //localStorage2();
+
+/* For Logging into the app */
+var loggedInUser = Backendless.UserService.loginSync( login, password, stayLoggedIn );
+var user;
+
+try
+{
+  user = Backendless.UserService.loginSync( username, password );
+}
+catch( err ) // see more on error handling
+{
+  console.log( "error message - " + err.message );
+  console.log( "error code - " + err.statusCode );
+}
+
+var userTokenIsValid = Backendless.UserService.isValidLoginSync(); 
+console.log( "Is login valid?: " +  userTokenIsValid );
+
 
 
 
