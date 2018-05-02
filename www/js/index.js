f7a4125c6be1c77652cdf5c6e0a55402d24ac1bb
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-05-02 20:27:09 +0100

    update

--- www/js/index.js
+++ www/js/index.js
@@ -243,20 +243,21 @@ function failPosition(error) {
 
 // Author: https://www.geodatasource.com/developers/javascript
     function distance(lat1, lon1, lat2, lon2, unit) {
-        console.log("lat1: " + lat1)
-        console.log("long1: " + lon1)
-        console.log("eventlat: " + lat2)
-        console.log("eventlat: " + lon2)
-	   var radlat1 = Math.PI * lat1/180;
-	   var radlat2 = Math.PI * lat2/180;
-	   var theta = lon1-lon2;
-	   var radtheta = Math.PI * theta/180;
-	   var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	   dist = Math.acos(dist);
-	   dist = dist * 180/Math.PI;
-	   dist = dist * 60 * 1.1515;
-        if (unit=="K") { dist = dist * 1.609344 
-                       console.log("dist: " + dist);
-                       }
-        return dist;
+        console.log("eventlat: " + lat1)
+        console.log("eventlong: " + lon1)
+        console.log("lat2: " + lat2)
+        console.log("long2: " + lon2)
+        var R = 6371; // km (change this constant to get miles)
+	   var dLat = (lat2-lat1) * Math.PI / 180;
+	   var dLon = (lon2-lon1) * Math.PI / 180;
+	   var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+		Math.sin(dLon/2) * Math.sin(dLon/2);
+	   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	   var d = R * c;
+	   if (d>1) return Math.round(d)+"km";
+	   else if (d<=1) return Math.round(d*1000)+"m";
+        
+        console.log("distance: " + d)
+	   return d;
     }
