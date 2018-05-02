d02179c1f56dcbcfbd68c898d42b2b8f94e9d1b4
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2018-05-02 23:01:17 +0100

    hgfx

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,4 @@
+document.addEventListener("deviceready", onDeviceReady, false);
 /*Backendless: https://backendless.com/docs/js/doc.html#welcome*/
 Backendless.initApp("BEC3A11B-2A08-4B8F-FF29-4F33380A3900","3C8378C2-F2B4-F4C9-FFCE-F0C165EBFB00"); //AppID then JS API key
 
@@ -24,10 +25,6 @@ var dataQueryBuilder = Backendless.DataQueryBuilder.create()
 dataQueryBuilder.setSortBy( ["created"] );
 $(document).on("pageshow","#homePage", onPageShow); //When home page shows
 $(document).on("click", "#addConfirmButton", onAddEvent);
-
-   function onLoad() {
-        document.addEventListener("deviceready", onDeviceReady, false);
-    }
 
 function onDeviceReady(){
  	console.log("device ready");
